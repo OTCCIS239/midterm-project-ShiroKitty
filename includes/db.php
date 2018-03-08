4a29c87b730134868c96614f2b7dfc27100abc41
LIGGETT <liggettt@otc.edu>
2018-03-08 10:30:04 -0600

    Trying to fix Heroku

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,7 @@
 <?php
 
 $host = getenv('DB_HOST');
-// $port = getenv('DB_PORT');
-$port = 53583;
+$port = getenv('DB_PORT');
 $database = getenv('DB_DATABASE');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
